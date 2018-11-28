4a276a8844de366113b51d4f07bf9ff3b43c336c
Abhinav Choudhury <achoudh3@ncsu.edu>
2018-11-27 23:10:53 -0500

    Added Facebook Login

--- app/src/main/java/app/traveler/com/travelerapp/LaunchActivity.java
+++ app/src/main/java/app/traveler/com/travelerapp/LaunchActivity.java
@@ -2,6 +2,9 @@ package app.traveler.com.travelerapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.widget.LoginButton;
 
 public class LaunchActivity extends AppCompatActivity {
 
@@ -9,5 +12,9 @@ public class LaunchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launch);
+
+        final LoginButton fbLoginButton = (LoginButton) findViewById(R.id.login_button);
+        fbLoginButton.setBackgroundResource(R.drawable.facebook);
+        fbLoginButton.setCompoundDrawablesWithIntrinsicBounds(0,0,0,0);
     }
 }
